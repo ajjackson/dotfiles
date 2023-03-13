dc712f5415957f06ff3c589877836ce969e8411c
Adam J. Jackson <a.j.jackson@physics.org>
2023-03-13 14:49:02 +0000

    Rework elisp file into "launch script", detection can live in org

--- get-environment.el
+++ get-environment.el
@@ -1,8 +1,14 @@
-(setq system
-      (let ((hostname (system-name)))
-        (cond ((member hostname '("Angel" "Angel.lan" "Angel.local")) "Angel")
-              ((member hostname '("ajj-mbp-1" "ajj-mbp-1.local"
-                                  "dock-ajj-mbp-1" "dock-ajj-mbp-1.esc.rl.ac.uk"))
-               "ajj-mbp-1")
-              ('t hostname))
-        ))
+#! /usr/bin/env -S emacs -Q --script
+
+(let* ((org-confirm-babel-evaluate nil)
+       (this-file (or load-file-name buffer-file-name))
+       (this-dir (file-name-directory this-file))
+       (dotfiles-org (expand-file-name "dotfiles.org" this-dir)))
+  (find-file dotfiles-org)
+  (org-babel-goto-named-src-block "guess-system")
+  (org-babel-execute-src-block)
+  (org-babel-tangle)
+
+  )
+
+(setq argv nil) ; Prevent emacs from trying to visit remaining arguments as files
