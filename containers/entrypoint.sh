e36fc7745e3c9c64b0b98dbc06874240bb5b32ab
Adam J. Jackson <a.j.jackson@physics.org>
2025-04-25 15:26:31 +0100

    Remove unused (micromamba) container, update README

--- containers/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-source /usr/local/bin/_entrypoint.sh
-cd "$HOME/src/dotfiles" || exit
-./make.el
-rm ~/.bashrc
-stow --dotfiles --target $HOME emacs bash fish git ssh tmux
-bash -l
